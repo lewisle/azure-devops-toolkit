9c2d0c448bbfa64c2f2e874460e7fe4e2c30bf5a
Lewis LE <phuonghoangle@gmail.com>
2021-02-27 12:11:49 +0000

    Add trash and copy buttons

--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,39 @@
 window.onload = function () {
     const clipboard = document.getElementById('clipboard');
+    const optionsButton = document.getElementById('options');
+    const clearButton = document.getElementById('clear');
+    const showNoItems = () => {
+        clipboard.innerHTML = '<div class="item">No items found ¯(°_o)/¯</div>';
+        clearButton.style.display = 'none';
+    };
 
     chrome.storage.sync.get(null, (result) => {
-        if (!result || !result.items) {
-            clipboard.innerHTML = '<div class="item">No items found ¯(°_o)/¯</div>';
+        if (!result || !result.items || !result.items.length) {
+            showNoItems();
             return;
         }
 
         const items = result.items.map((item) => {
-            return `<div class="item">${item}</div>`;
+            return `<div class="item">
+                <div class="text">${item}</div>
+                <!--<div class="actions">
+                    <button class="copy">✔ Copy</button>
+                    <button class="delete">❌ Trash</button>
+                </div>-->
+            </div>`;
         });
         clipboard.innerHTML = items.join('');
+
+        clearButton.onclick = function () {
+            if (confirm('Are you sure you want to clear all items?')) {
+                chrome.storage.sync.set({ items: [] }, () => {
+                    showNoItems();
+                });
+            }
+        };
     });
 
-    const settings = document.getElementById('settings');
-
-    settings.onclick = function () {
+    optionsButton.onclick = function () {
         chrome.runtime.openOptionsPage();
     };
 };
