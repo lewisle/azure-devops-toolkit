9b6e718948e98a0d615492375711bab1447dabae
Lewis LE <phuonghoangle@gmail.com>
2021-02-28 11:17:10 +0000

    Add `Copy`, `Trash` and `Notification`

--- popup/popup.js
+++ popup/popup.js
@@ -1,39 +1,90 @@
 window.onload = function () {
-    const clipboard = document.getElementById('clipboard');
-    const optionsButton = document.getElementById('options');
+    const itemsContainer = document.getElementById('clipboard');
     const clearButton = document.getElementById('clear');
-    const showNoItems = () => {
-        clipboard.innerHTML = '<div class="item">No items found ¯(°_o)/¯</div>';
-        clearButton.style.display = 'none';
-    };
+    const optionsButton = document.getElementById('options');
+    const notificationContainer = document.getElementById('notification');
 
-    chrome.storage.sync.get(null, (result) => {
+    optionsButton.onclick = () => chrome.runtime.openOptionsPage();
+    chrome.storage.sync.get(null, renderItems);
+
+    function renderItems(result) {
+        itemsContainer.innerHTML = '';
+
         if (!result || !result.items || !result.items.length) {
             showNoItems();
             return;
         }
 
-        const items = result.items.map((item) => {
-            return `<div class="item">
-                <div class="text">${item}</div>
-                <!--<div class="actions">
-                    <button class="copy">✔ Copy</button>
-                    <button class="delete">❌ Trash</button>
-                </div>-->
-            </div>`;
+        result.items.forEach((item, index, items) => {
+            const text = createElement('div', 'text', [createText(item)]);
+
+            const copyButton = createButton('copy', 'Copy to clipboard', '✅', () => {
+                if (!navigator || !navigator.clipboard) {
+                    return;
+                }
+
+                navigator.clipboard.writeText(item).then(
+                    () => {
+                        setNotification('Copied 👍', 2000);
+                    },
+                    (err) => {
+                        console.error('ADOT: Could not copy text: ', err);
+                    },
+                );
+            });
+
+            const trashButton = createButton('trash', 'Trash', '❌', () => {
+                items.splice(index, 1);
+                const newItems = { items };
+                chrome.storage.sync.set(newItems);
+                renderItems(newItems);
+            });
+
+            const actionsContainer = createElement('div', 'actions', [copyButton, trashButton]);
+            const itemContainer = createElement('div', 'item');
+            itemContainer.append(text, actionsContainer);
+            itemsContainer.append(itemContainer);
         });
-        clipboard.innerHTML = items.join('');
 
         clearButton.onclick = function () {
             if (confirm('Are you sure you want to clear all items?')) {
-                chrome.storage.sync.set({ items: [] }, () => {
-                    showNoItems();
-                });
+                chrome.storage.sync.set({ items: [] }, () => showNoItems());
             }
         };
-    });
+    }
 
-    optionsButton.onclick = function () {
-        chrome.runtime.openOptionsPage();
-    };
+    function setNotification(message, timeout) {
+        notificationContainer.innerHTML = message;
+        notificationContainer.className = 'on';
+
+        window.setTimeout(() => {
+            notificationContainer.className = '';
+            notificationContainer.innerHTML = '';
+        }, timeout);
+    }
+
+    function showNoItems() {
+        itemsContainer.innerHTML = '<div class="item">No items found ¯(°_o)/¯</div>';
+        clearButton.style.display = 'none';
+    }
+
+    function createButton(className, title, value, onClickHandler) {
+        const button = createElement('button', className, [createText(value)]);
+        button.setAttribute('title', title);
+        button.onclick = onClickHandler;
+        return button;
+    }
+
+    function createText(value) {
+        return document.createTextNode(value);
+    }
+
+    function createElement(tagName, className, children) {
+        const element = document.createElement(tagName);
+        element.className = className;
+        if (children) {
+            element.append(...children);
+        }
+        return element;
+    }
 };
